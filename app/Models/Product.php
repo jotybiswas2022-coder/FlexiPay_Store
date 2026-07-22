14d97871976958aced202f1054e5f18e7a767d6b
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-07-22 17:37:57 +0600

    Fix reviews relationship - use morphMany for polymorphic reviewable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -68,6 +68,6 @@ class Product extends Model
 
     public function reviews()
     {
-        return $this->hasMany(Review::class);
+        return $this->morphMany(Review::class, 'reviewable');
     }
 }
