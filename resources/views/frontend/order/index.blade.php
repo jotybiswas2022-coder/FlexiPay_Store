e51ddc475787ac7a953ebe69a9174fdb43667269
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-07-23 23:35:34 +0600

    Add alt text to order item images in order index

--- resources/views/frontend/order/index.blade.php
+++ resources/views/frontend/order/index.blade.php
@@ -144,7 +144,7 @@
                 @foreach($order->items->take(3) as $item)
                 <div class="fp-order-item">
                     @if($item->product && $item->product->primaryImage)
-                        <img src="{{ asset('storage/'.$item->product->primaryImage->image_path) }}" alt="">
+                        <img src="{{ asset('storage/'.$item->product->primaryImage->image_path) }}" alt="{{ $item->product->name }}">
                     @else
                         <div class="fp-oi-no-img"><i class="bi bi-image"></i></div>
                     @endif
